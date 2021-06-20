cf234cf6038b58b4abd2b18c875a38fae170d1a9
hisho <hisho.web@gmail.com>
2021-06-21 03:50:28 +0900

    🐛 fixed

--- src/percentage.ts
+++ src/percentage.ts
@@ -1,10 +1,6 @@
 import {percentageType} from "./types";
 
-function orgRound(value: number, base: number = 1000) {
-  return Math.round(value * base) / base;
-}
-
 // Converts a unit less $number (usually a decimal between 0 and 1) to a percentage.
 export const percentage: percentageType = ($number) => {
-  return `${(orgRound($number) * 100)}%` as const;
+  return `${Number(($number * 100).toFixed(4))}%` as const;
 };
