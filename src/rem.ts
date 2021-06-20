38f2f5d41e81a72ba49f80347cf16828ca039290
hisho <hisho.web@gmail.com>
2021-06-21 02:55:30 +0900

    🐛 fixed rem

--- src/rem.ts
+++ src/rem.ts
@@ -1,6 +1,6 @@
 import {remType} from "./types";
 
 // Converts a unit less $number to a rem.
-export const rem: remType = ($number) => {
-  return `${$number / 16}rem` as const;
+export const rem: remType = ($number, root = 16) => {
+  return `${$number / root}rem` as const;
 };
